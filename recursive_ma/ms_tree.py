4b102b4ee7457c987e3d4d26b58802385da96c1e
Hessam Mehr <Hessam.Mehr@glasgow.ac.uk>
2023-04-28 17:14:27 +0000

    Format using black.

--- recursive_ma/ms_tree.py
+++ recursive_ma/ms_tree.py
@@ -1,19 +1,21 @@
-
 def _build_tree(data, level=1, acc=None, parent=None, max_level=3):
     max_level = min(max_level, max(data))
     if level == 1:
         acc = {}
-        for peak in data[1]['mz']:
+        for peak in data[1]["mz"]:
             acc[peak] = {}
             _build_tree(data, level=2, acc=acc[peak], parent=peak, max_level=max_level)
         return acc
     level_df = data[level]
-    parent_df = data[level - 1].drop(columns=['parent_id'], errors='ignore')
-    level_df = level_df.join(parent_df, on='parent_id', rsuffix='_parent')
-    for peak in level_df[level_df[f'mz_parent'] == parent][f'mz']:
+    parent_df = data[level - 1].drop(columns=["parent_id"], errors="ignore")
+    level_df = level_df.join(parent_df, on="parent_id", rsuffix="_parent")
+    for peak in level_df[level_df[f"mz_parent"] == parent][f"mz"]:
         acc[peak] = None if level == max_level else {}
         if level < max_level:
-            _build_tree(data, level=level+1, acc=acc[peak], parent=peak, max_level=max_level)
+            _build_tree(
+                data, level=level + 1, acc=acc[peak], parent=peak, max_level=max_level
+            )
+
 
 def build_tree(data, max_level=3):
     return _build_tree(data, max_level=max_level)
--- tests/test_recursive_ma.py
+++ tests/test_recursive_ma.py
@@ -7,30 +7,37 @@ from recursive_ma import build_tree, estimate_MA, estimate_by_MW, common_MA
 
 HERE = Path(__file__).parent
 
+
 @pytest.fixture
 def pickle_files():
     data_dict = {
         # filename: 'Sample..._ms<n>.pkl' => n
-        int(pickle_file.name.split('_')[1][2]): pickle.load(pickle_file.open('rb'))
-        for pickle_file in sorted(HERE.glob('*.pkl'))
+        int(pickle_file.name.split("_")[1][2]): pickle.load(pickle_file.open("rb"))
+        for pickle_file in sorted(HERE.glob("*.pkl"))
     }
 
     # rename ms<n>_mz columns to mz and ms<n>_intensity to intensity
     for level, data in data_dict.items():
-        data.rename(columns={f'ms{level}_mz': 'mz', f'ms{level}_intensity': 'intensity'}, inplace=True)
-    
+        data.rename(
+            columns={f"ms{level}_mz": "mz", f"ms{level}_intensity": "intensity"},
+            inplace=True,
+        )
+
     return data_dict
+
 
 @pytest.fixture
 def test_data(pickle_files, request):
     level = request.param
     return build_tree(pickle_files, max_level=level)
 
+
 @pytest.fixture
 def mock_data():
     # Mock MS3 data (could go higher).
     # {} means ion did not fragment; None means didn't try fragmenting ion.
     return {371.2: {150.1: {72.3: None, 89.1: None}, 221.3: {72.35: None, 99.7: None}}}
+
 
 def test_mock_data(mock_data):
     parent_ma = estimate_MA(mock_data, 371.2)
@@ -39,8 +46,9 @@ def test_mock_data(mock_data):
     common_ma = common_MA(mock_data[371.2], 150.1, 221.3)
     assert parent_ma == child1_ma + child2_ma - common_ma
 
+
 # test_data parameterised on ms_level
-@pytest.mark.parametrize('test_data', [1, 2, 3, 4], indirect=True)
+@pytest.mark.parametrize("test_data", [1, 2, 3, 4], indirect=True)
 def test_real_data(test_data, request):
     mw = list(test_data)[0]
     ma = estimate_MA(test_data, mw)
