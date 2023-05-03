d7bad4e209b4ba4dc654da3de48a79215314225d
Hessam Mehr <Hessam.Mehr@glasgow.ac.uk>
2023-05-03 17:04:34 +0000

    build_tree: Respect max_level.

--- recursive_ma/ms_tree.py
+++ recursive_ma/ms_tree.py
@@ -6,15 +6,14 @@ def _build_tree(data, level=1, acc=None, parent=None, max_level=3):
             acc[peak] = {}
             _build_tree(data, level=2, acc=acc[peak], parent=peak, max_level=max_level)
         return acc
+    if level > max_level:
+        return
     level_df = data[level]
     parent_df = data[level - 1].drop(columns=["parent_id"], errors="ignore")
     level_df = level_df.join(parent_df, on="parent_id", rsuffix="_parent")
     for peak in level_df[level_df[f"mz_parent"] == parent][f"mz"]:
         acc[peak] = None if level == max_level else {}
-        if level < max_level:
-            _build_tree(
-                data, level=level + 1, acc=acc[peak], parent=peak, max_level=max_level
-            )
+        _build_tree(data, level=level + 1, acc=acc[peak], parent=peak, max_level=max_level)
 
 
 def build_tree(data, max_level=3):
