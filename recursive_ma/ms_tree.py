182c42583a83ff4c91f50d5eb280079d8d9bd2a0
Hessam Mehr <Hessam.Mehr@glasgow.ac.uk>
2023-05-12 10:17:56 +0000

    Document tree builder, add `tree_depth` function.

--- recursive_ma/ms_tree.py
+++ recursive_ma/ms_tree.py
@@ -19,5 +19,35 @@ def _build_tree(data, level=1, acc=None, parent=None, max_level=3):
         )
 
 
-def build_tree(data, max_level=3):
+def build_tree(data: dict, max_level=3):
+    """
+    Build a tree from a dictionary of {level: DataFrame, ...}.
+
+    Args:
+        data (dict): {level: DataFrame, ...}.
+        max_level (int): maximum MS level to include in the tree, e.g. 3 for MS3.
+    """
     return _build_tree(data, max_level=max_level)
+
+
+def tree_depth(tree: dict):
+    """
+    Calculate the level of nesting in a tree.
+    
+    >>> tree_depth({})
+    0
+    >>> tree_depth({1: None})
+    1
+    >>> tree_depth({1: {}})
+    1
+    >>> tree_depth({1: {2: {3: None}}})
+    3
+    >>> tree_depth({1: {2: {3: None}, 4: None}})
+    3
+    >>> tree_depth({1: {2: {3: None}, 4: {5: None}}})
+    4
+    """
+    if isinstance(tree, dict) and len(tree) > 0:
+        return 1 + max(tree_depth(v) for v in tree.values())
+    else:
+        return 0
